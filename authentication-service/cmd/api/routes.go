37b4b0d37c10fe28bacfdc88aacfd02c86b05107
Andrei Koshelap <andreikoshelap@gmail.com>
2025-02-16 23:05:27 +0200

    fixed authentication vulnerabilities

--- authentication-service/cmd/api/routes.go
+++ authentication-service/cmd/api/routes.go
@@ -22,8 +22,8 @@ func (app *Config) routes() http.Handler {
 		MaxAge: 300,
 	}))
 
-	mux.Use(middleware.Heartbeat("/ping"));
-	mux.Post("/authenticate", app.Authenticate);
+	mux.Use(middleware.Heartbeat("/ping"))
 
+	mux.Post("/authenticate", app.Authenticate)
 	return mux
 }
